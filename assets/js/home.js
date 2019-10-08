bc29bba81f4d71040b81b8bd64eb3e5d2a1fc624
unknown <nkumarnet40@gmail.com>
2019-10-08 22:55:20 +0530

    added transfer section

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+var button= document.getElementById('trf');
+button.addEventListener('click',toogle);
+
+function toogle(){
+    document.getElementById('catogery-div').style.display=none;
+    document.getElementById('trf-div').style.display=block;
+
+}
