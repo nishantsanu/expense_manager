eaea673d0f512c08665b1e695d8f51a06baaee6d
unknown <nkumarnet40@gmail.com>
2019-10-09 20:05:28 +0530

    added proc file

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,13 @@
-var button= document.getElementById('trf');
-button.addEventListener('click',toogle);
-
-function toogle(){
-    document.getElementById('catogery-div').style.display=none;
-    document.getElementById('trf-div').style.display=block;
+// var but= document.getElementById('pymntMtd');
+// console.log(but);
+function toggle(val){
+    console.log(val.value);
+    if(val.value == 'trf'){    
+        document.getElementById('catogery-div').style.display='none';
+        document.getElementById('trf-div').style.display='block';
+    }
 
 }
+
+// but.addEventListener('change',toggle);
+
